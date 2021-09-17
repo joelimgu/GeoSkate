cc4a8e676cc170c2194c70c31dee2e97f349afd2
ImPuLsE213 <47368268+ImPuLsE213@users.noreply.github.com>
2021-09-17 13:40:56 +0200

    Landing page & Login page & Register page done [WITH CSS!!!] Must change in .TS file if want to work with SASS (and modify code of course)

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.sass']
+  styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
 
